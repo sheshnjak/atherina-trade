5f498d3faf64ef8f62edb47defbfe21853b5abc2
sheshnjak <sheshnjak@gmail.com>
2016-10-17 15:10:06 +0200

    Add files via upload

---
+++ dizajn/js.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function(){
+if($.browser.msie && $.browser.version < 7){$("#zaglavlje").css("background-image","none"); // ie6 transparency international, jeba ih bilgejts
+$("#naslov").css("padding-left","40px");}
+
+if(window.location.pathname.length>2){
+$('ul#navigacija>li>a[href$="'+location.pathname.substring(1).substring(location.pathname.substring(1).lastIndexOf("/")+1)+'"]').addClass("aktivna");
+$('div#podnozje>a[href$="'+location.pathname.substring(1).substring(location.pathname.substring(1).lastIndexOf("/")+1)+'"]').addClass("trenutna");}
+$.emajl();
+
+// testis zona
+
+$("#slideShow").nivoSlider({pauseTime:4500});
+$("#brodovi").nivoSlider({pauseTime:4500});
+});
+jQuery.extend({	// stare metode prebacene u $.namespace
+emajl: function(){  // <a class="emajl" href="user_nameATdomain">tekst linka</a> >>> mailto:user_name@domain
+$("a.emajl").each(function(){
+$(this).attr("href",$(this).attr("href").replace(/([^A]+)AT([\w]+)/,"mailto:$1@$2"));
+if(!$(this).text()) $(this).text($(this).attr("href").substr(7));
+});}
+// testis zona, dodaj zarez prije nove funkcije
+
+
+});
